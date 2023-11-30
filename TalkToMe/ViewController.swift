dfaf7ec6bf70382ed752ef94a23bc325cb05d19e
Johan Kool <johankool@users.noreply.github.com>
2023-11-30 13:36:52 +0100

    Continue in background and highlight current word

--- TalkToMe/ViewController.swift
+++ TalkToMe/ViewController.swift
@@ -17,6 +17,11 @@ class ViewController: UIViewController {
     }
     
     var utterances: [AVSpeechUtterance] = []
+    var highlightedRange: (Int, NSRange)? {
+        didSet {
+            updateHighlight(oldValue: oldValue)
+        }
+    }
     
     @IBOutlet var textLabel: UILabel!
     @IBOutlet var textLabel2: UILabel!
@@ -39,15 +44,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func speakButtonTapped(_ sender: Any) {
-        print("Speaking!")
-        let utterance = AVSpeechUtterance(attributedString: textLabel.attributedText!)
+        print("Spreek!")
+        let session = AVAudioSession.sharedInstance()
+        do {
+            // 1) Configure your audio session category, options, and mode
+            try session.setCategory(.playback)
+            // 2) Activate your audio session to enable your custom configuration
+            try session.setActive(true)
+        } catch let error as NSError {
+            print("Unable to activate audio session:  \(error.localizedDescription)")
+        }
         switch status {
         case .ready:
             utterances = [textLabel, textLabel2, textLabel3]
                 .map(\.attributedText)
                 .map(utterance(for:))
+            status = .preparing
             synthesizer.speak(utterances[0])
-        case .speaking:
+        case .preparing, .speaking:
             break
         case .paused:
             synthesizer.continueSpeaking()
@@ -56,7 +70,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func pauseButtonTapped(_ sender: Any) {
-        print("Pauze!")
+        print("Pauze")
         synthesizer.pauseSpeaking(at: .word)
     }
     
@@ -64,6 +78,9 @@ class ViewController: UIViewController {
         switch status {
         case .ready:
             speakButton.isHidden = false
+            pauseButton.isHidden = true
+        case .preparing:
+            speakButton.isHidden = true
             pauseButton.isHidden = true
         case .speaking:
             speakButton.isHidden = true
@@ -73,10 +90,41 @@ class ViewController: UIViewController {
             pauseButton.isHidden = true
         }
     }
+    
+    private func updateHighlight(oldValue: (Int, NSRange)?) {
+        func label(for index: Int) -> UILabel {
+            let label: UILabel!
+            if index == 0 {
+                label = textLabel
+            } else if index == 1 {
+                label = textLabel2
+            } else if index == 2 {
+                label = textLabel3
+            } else {
+                fatalError()
+            }
+            return label
+        }
+        if let (oldIndex, oldRange) = oldValue {
+            let label = label(for: oldIndex)
+            if let attributedText = label.attributedText?.mutableCopy() as? NSMutableAttributedString {
+                attributedText.removeAttribute(NSAttributedString.Key.backgroundColor, range: oldRange)
+                label.attributedText = attributedText
+            }
+        }
+        if let (newIndex, newRange) = highlightedRange {
+            let label = label(for: newIndex)
+            if let attributedText = label.attributedText?.mutableCopy() as? NSMutableAttributedString {
+                attributedText.addAttribute(NSAttributedString.Key.backgroundColor, value: UIColor.blue.cgColor, range: newRange)
+                label.attributedText = attributedText
+            }
+        }
+    }
 }
 
 enum SynthesizerStatus {
     case ready
+    case preparing
     case speaking
     case paused
 }
@@ -111,20 +159,7 @@ extension ViewController: AVSpeechSynthesizerDelegate {
     
     func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) {
         let index = utterances.firstIndex(of: utterance)!
-        let label: UILabel!
-        if index == 0 {
-            label = textLabel
-        } else if index == 1 {
-            label = textLabel2
-        } else if index == 2 {
-            label = textLabel3
-        } else {
-            fatalError()
-        }
-        if let attributedText = label.attributedText?.mutableCopy() as? NSMutableAttributedString {
-            attributedText.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: characterRange)
-            label.attributedText = attributedText
-        }
+        highlightedRange = (index, characterRange)
     }
     
 }
